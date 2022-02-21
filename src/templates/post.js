b39ce7e03da7f6ba4367ab25a94969a6540697ef
Amod Kumar <96678063+h0p316@users.noreply.github.com>
2022-02-21 12:03:01 +0530

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -43,7 +43,7 @@ const PostTemplate = ({ data, location }) => {
     <Layout location={location}>
       <Helmet>
         <title>{title} | Amod Kumar</title>
-        <link rel="canonical" href="https://chandrikadeb7.github.io/pensieve" />
+        <link rel="canonical" href="https://h0p316.github.io/pensieve" />
       </Helmet>
 
       <StyledPostContainer>
